51b376fa8e5b207dc4b47e9112a1d19d5573b9fb
smych <smychenkoiv@gmail>
2025-09-20 23:59:24 +0300

    format fives homework

--- src/main/kotlin/org/deploys/three/lessons/lesson05/homeworks/ElvisOperator.kt
+++ src/main/kotlin/org/deploys/three/lessons/lesson05/homeworks/ElvisOperator.kt
@@ -1,6 +1,6 @@
 package org.deloys.three.org.deploys.three.lessons.lesson05.homeworks
 
-fun main () {
+fun main() {
 // Задача 1
 // Контекст: Вы изучаете физическое явление затухания звука в помещении. У вас есть измеренное значение начальной
 // интенсивности звука, но из-за ограничений оборудования данные о коэффициенте затухания иногда могут быть неизвестным.
@@ -9,7 +9,7 @@ fun main () {
 // Если коэффициент затухания неизвестен, использовать стандартное значение 0.5.
 
     val initSoundIntensive: Double = 58.0
-    val soundAttenuationCaf: Double? = 0.11
+    val soundAttenuationCaf: Double = 0.11
     val defaultAttenuationCaf: Double = 0.5
     val soundIntensiveAfterAttenuation = initSoundIntensive * (soundAttenuationCaf ?: defaultAttenuationCaf)
     println(soundIntensiveAfterAttenuation)
@@ -21,8 +21,7 @@ fun main () {
 // Задача: Рассчитать полную стоимость доставки.
 
     val defaultCost: Double = 50.0
-    val generalCost: Double? = 40.0
-    val deliveryCost = 5.0
+    val generalCost: Double = 40.0
     val cargoInsurance: Double = 0.005
     val cargoInsuranceCost = (generalCost ?: defaultCost) * cargoInsurance
     val fullPrice: Double = cargoInsuranceCost + defaultCost
@@ -34,7 +33,7 @@ fun main () {
 // которое должно быть зафиксировано. Лаборант приносит вам набор показателей, но по пути может что-нибудь потерять.
 // Задача - сообщить об ошибке в случае отсутствия показаний атмосферного давления.
 
-    val atmosphericPressure: String? = "756.0"
+    val atmosphericPressure: String = "756.0"
     val lostPressureMetric: String = "Lost the pressure metric!"
     val pressureInLab = atmosphericPressure ?: lostPressureMetric
     println(pressureInLab)
