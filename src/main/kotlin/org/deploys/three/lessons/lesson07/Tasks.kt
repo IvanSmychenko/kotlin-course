6f4dfe211ddadd91bc91b1bb9ed2399fe07a5ab0
ivan.sm <ivan.sm@itechpsp.com>
2025-09-24 17:00:21 +0300

    lesson 7

--- src/main/kotlin/org/deploys/three/lessons/lesson07/Tasks.kt
+++ src/main/kotlin/org/deploys/three/lessons/lesson07/Tasks.kt
@@ -10,6 +10,43 @@ fun main() {
 
     val length: Int = 34
     for (i in 0 until length) {
-        println(length)
+        println(i)
+    }
+
+    var counter: Int = 0
+    var sum: Int = 0
+    while (counter++ < 10) {
+        sum+=counter
+        counter++
+    }
+    println(sum)
+
+    var counter2: Int = 0
+    var sum2: Int = 0
+    do {
+        counter2 ++
+        sum2+=counter2
+        println(sum2)
+    } while ((sum2 + counter2) <= 50)
+
+    var counter3: Int = 1
+    while (true) {
+        println(counter3)
+        if (counter3++ % 7 == 0) break
+    }
+
+    for (i in 1..10) {
+        if (i == 3 || i == 7) continue
+        println(i)
+    }
+
+    var counter4: Int = 0
+    for (i in 20 downTo 1) {
+        counter4++
+        if (counter4 == 4) {
+            counter4 = 0
+            continue
+        }
+        println(i)
     }
 }
